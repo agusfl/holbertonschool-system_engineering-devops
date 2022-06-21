e6d8a4ecc97cd88103d142edc0f52aef10846602
agusfl <agusflom@hotmail.com>
2022-06-21 14:40:47 +0000

    more comments

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# Script to match "hbtn" and the t can be between 1 and 4 times.
+# Script to match "hbtn" and the t can be between 1 and 4 times without using square brackets
 # Con el signo de ? chequeamos que sea 'b' o 't' y usamos el ++ para decirle que matchee todas las letras que vengan despues hasta la 'n'
 
 puts ARGV[0].scan(/(hbt?++n)/).join
