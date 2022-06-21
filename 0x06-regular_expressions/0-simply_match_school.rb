d22724e4765458a61e8503167f99168b2d9a9a3d
agusfl <agusflom@hotmail.com>
2022-06-21 23:39:44 +0000

    fix

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -3,4 +3,4 @@
 # Se usa el lenguaje ruby con el codigo que nos pasan y solo cambiamos la parte
 # de Regex, use esta pagina para probar el codigo: https://rubular.com/
 
-puts ARGV[0].scan(/[School]/).join
+puts ARGV[0].scan(/School/).join
