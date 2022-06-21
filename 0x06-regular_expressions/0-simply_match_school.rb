271e3d866ccd126603e4d5a937e879a48d0aad5f
agusfl <agusflom@hotmail.com>
2022-06-21 12:22:25 +0000

    task 0 with some comments

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# Script to match "School"
+# Se usa el lenguaje ruby con el codigo que nos pasan y solo cambiamos la parte
+# de Regex, use esta pagina para probar el codigo: https://rubular.com/
+
+puts ARGV[0].scan(/[School]/).join
