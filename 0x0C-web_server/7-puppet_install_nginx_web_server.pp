debd4993be8f1a13ffe9cae899ea2b520de00e85
agusfl <agusflom@hotmail.com>
2022-08-17 02:15:58 +0000

    task 6 - advanced

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,21 +4,20 @@
 
 # Install nginx:
 package { 'nginx':
+  ensure => 'installed'
 }
 
 # Se agrega Hello World en el archivo --> index.nginx-debian.html para mostrarlo en la pagina web
 file { '/var/www/html/index.nginx-debian.html':
-  ensure  => file,
-  mode    => '0744',
   content => 'Hello World!',
 }
 
 # Redirection 301 permanently
-file_line { 'redirect 301 status':
+file_line { 'redirect_301_status':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _;',
-  line   => 'a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 # Se indica el estado en el que tiene que estar el servicio (en este caso queremos que este corriendo)
