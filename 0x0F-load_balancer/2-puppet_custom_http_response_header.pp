81a8993c69c0bd402ac93a2b02316345bf2ab94f
agusfl <agusflom@hotmail.com>
2022-08-22 17:55:57 +0000

    fix - adding ''

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -10,7 +10,7 @@ file_line { 'add_path':
   ensure => present,
   path   => '/etc/nginx/sites-enabled/default',
   after  => 'listen 80 default_server;',
-  line   => "add_header X-Served-By ${hostname};",
+  line   => "add_header X-Served-By '${hostname}'';",
 }
 
 # Restart Nginx para que queden los cambios
