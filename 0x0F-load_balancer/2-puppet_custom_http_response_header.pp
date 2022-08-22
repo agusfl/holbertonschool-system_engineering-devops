3be927c75a22301890860bf6e95d27e4efdc2c98
agusfl <agusflom@hotmail.com>
2022-08-22 18:05:24 +0000

    try fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,27 @@
 # Just as in task #0, we’d like you to automate the task of creating a custom HTTP header response, but with Puppet.
 
+# update
+exec { 'update':
+  command  => 'sudo apt-get update',
+  provider => shell,
+}
+
 # Install nginx:
 package { 'nginx':
   ensure => 'installed'
+}
+
+# Se agrega Hello World en el archivo --> index.nginx-debian.html para mostrarlo en la pagina web
+file { '/var/www/html/index.nginx-debian.html':
+  content => 'Hello World!',
+}
+
+# Redirection 301 permanently
+file_line { 'redirect_301_status':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 # Add Header
