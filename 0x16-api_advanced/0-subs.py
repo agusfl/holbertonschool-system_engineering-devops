b0424a7851b3348b75629e1c4395a8f39691ff29
agusfl <agusflom@hotmail.com>
2022-09-21 14:52:33 +0000

    changing for a try and except

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -28,35 +28,34 @@ def number_of_subscribers(subreddit):
     """
     Count number of subreddits
     """
-
-    # Esta va a ser la URL a la que vamos a hacer una request, subreddit va a
-    # ser el "tema" que se pase como argumento en linea para retornar la
-    # cantidad de subscriptores de dicho tema.
-    url_subredit = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
-    headers = requests.utils.default_headers()
-
-    # Seteamos el User-Agent para no tener problemas de "too many requests"
-    headers.update(
-        {
-            'User-Agent': 'My User Agent 1.0',
-        }
-    )
-
-    # Hacemos una request de la url indicada arriba con el metodo GET
-    res = requests.get(url_subredit, headers=headers)
-
-    # Pasar datos a JSON para obtener un diccionario de python y poder
-    # trabajar con los datos en python
-    json_res = res.json()
-
     # Chequeamos si la respuesta fue existosa (status code = 200) significa que
     # el subredit existe. Si la respuesta no fue existosa (no dio status code
     # 200) es porque no existe ese "tema", por lo tanto retornamos 0
-    if res.status_code == 200:
+    try:
+        # Esta va a ser la URL a la que vamos a hacer una request, subreddit va
+        # a ser el "tema" que se pase como argumento en linea para retornar la
+        # cantidad de subscriptores de dicho tema.
+        url_sub = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+        headers = requests.utils.default_headers()
+
+        # Seteamos el User-Agent para no tener problemas de "too many requests"
+        headers.update(
+            {
+                'User-Agent': 'My User Agent 1.0',
+            }
+        )
+
+        # Hacemos una request de la url indicada arriba con el metodo GET
+        res = requests.get(url_sub, headers=headers)
+
+        # Pasar datos a JSON para obtener un diccionario de python y poder
+        # trabajar con los datos en python
+        json_res = res.json()
+
         # Traemos el atributo data del diccionario "res" que nos devolvio el
         # .json y dentro del atributo data buscamos la cantidad de subscribers
         # Usamos la nomenclatura para acceder a diccionarios de python
         return json_res['data']['subscribers']
-    else:
+    except Exception:
         return 0
