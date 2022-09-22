b370dcaf4356162b8daf630220f9afdfe7f7b903
agusfl <agusflom@hotmail.com>
2022-09-22 01:41:45 +0000

    task 2 - with some comments

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -20,3 +20,45 @@ function! This /can/ be done with a loop but the point is to use a recursive
 function. :)
 """
 import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    Return the hot list
+    """
+    try:
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
+        headers = requests.utils.default_headers()
+
+        # Seteamos el User-Agent para no tener problemas de "too many requests"
+        headers.update({'User-Agent': 'My User Agent 1.0'})
+
+        # Hacemos una request de la url indicada arriba con el metodo GET
+        res = requests.get(url, headers=headers, params={'after': after},
+                           allow_redirects=False)
+
+        # Pasar datos a JSON para obtener un diccionario de python y poder
+        # trabajar con los datos en python
+        res_json = res.json()
+
+        child = res_json['data']['children']
+
+        # Se hace un append de cada dato dentro de child en la lista a retornar
+        for title in child:
+            hot_list.append(title['data']['title'])
+
+        # Con after se itera por todos los datos
+        after = res_json['data']['after']
+
+        # Mientras que haya paginas para recorrer se sigue con la recursion
+        if after is not None:
+            recurse(subreddit, hot_list, after)
+
+        # Si no se entra en el if de arriba y se retorna la hot_list es por
+        # que se cumplio la condicion base y se sale de la recursion
+        return hot_list
+
+    except Exception:
+        # If not a valid subreddit, return None.
+        return None
